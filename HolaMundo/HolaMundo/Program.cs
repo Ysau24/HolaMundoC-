5f0bafb66b2dd415617ce623e835d46efc836c45
Ysauri Jiménez <ysmajimo@gmail.com>
2025-06-18 18:19:30 -0400

    Primer commit

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System;
+
+namespace HolaMundo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo!");
+        }
+    }
+}
